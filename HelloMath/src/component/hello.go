4337e2b6d614f2413e597bcde4bda6ece9a6eba2
Xabc-d <jj19050@bristol.ac.uk>
2024-03-04 22:45:18 +0000

    nav

--- HelloMath/src/component/hello.go
+++ HelloMath/src/component/hello.go
@@ -4,12 +4,16 @@ import "github.com/maxence-charriere/go-app/v9/pkg/app"
 
 type Hello struct {
 	app.Compo
-	Name string
+	Sidebar Sidebar
+	Name    string
 }
 
 func (h *Hello) Render() app.UI {
 	return app.Main().Body(
 		&Header{},
-		&Sidebar{},
+		&h.Sidebar,
+		//&Sidebar{},
+		//&Calculator{},
+		&Week{Name: h.Sidebar.SelectedItem},
 	)
 }
--- HelloMath/src/component/menu.go
+++ HelloMath/src/component/menu.go
@@ -1,45 +1,62 @@
 package component
 
-import "github.com/maxence-charriere/go-app/v9/pkg/app"
+import (
+	"fmt"
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
 
 type Sidebar struct {
 	app.Compo
+	SelectedItem string
+}
+
+func (s *Sidebar) onClick(ctx app.Context, e app.Event) {
+	e.PreventDefault()
+	v := ctx.JSSrc().Get("text").String()
+	//select {}
+	fmt.Println(v)
+	s.SelectedItem = v
 }
 
 func (s *Sidebar) Render() app.UI {
-	return app.Div().Class("d-flex flex-column").Style("width", "20%").Body(
+	return app.Div().Class("d-flex").Body(
+		app.Div().Class("col-md-3 ").Body(
+			app.Div().Class("row").Body(
+				app.Div().Class("bg-light sidebar").Style("width", "100%").Style("flex-shrink", "0").Body(
+					app.Ul().Class("nav flex-column").Body(
+						app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
+							app.A().OnClick(s.onClick).Class("nav-link active").Href("/week01").Body(
+								app.Text("week01"),
+							),
+						),
 
-		app.Div().Class("row").Body(
-			app.Div().Class("bg-light sidebar").Style("width", "100%").Style("flex-shrink", "0").Body(
-				app.Ul().Class("nav flex-column").Body(
-					app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
-						app.A().Class("nav-link active").Href("#").Body(
-							app.Text("week01"),
+						app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
+							app.A().OnClick(s.onClick).Class("nav-link active").Href("#").Body(),
 						),
-					),
-					app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
-						app.A().Class("nav-link active").Href("#").Body(
-							app.Text("week02"),
+
+						app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
+							app.A().OnClick(s.onClick).Class("nav-link active").Href("#").Body(
+								app.Text("week03"),
+							),
 						),
-					),
-					app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
-						app.A().Class("nav-link active").Href("#").Body(
-							app.Text("week03"),
+						app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
+							app.A().OnClick(s.onClick).Class("nav-link active").Href("#").Body(
+								app.Text("week04"),
+							),
 						),
-					),
-					app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
-						app.A().Class("nav-link active").Href("#").Body(
-							app.Text("week04"),
-						),
-					),
-					app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
-						app.A().Class("nav-link active").Href("#").Body(
-							app.Text("week05"),
+						app.Li().Class("nav-item").Style("width", "50%").Style("margin", "0 auto").Body(
+							app.A().OnClick(s.onClick).Class("nav-link active").Href("#").Body(
+								app.Text("week05"),
+							),
 						),
 					),
 				),
-				//app.Div().Style("width", "30%").Body(),
 			),
 		),
+		//),
+		app.Div().Body(
+			app.Text("content"),
+		),
 	)
+
 }
